50e95417422d9f5e00ad5876fa64cb024eed675e
theyashpatel <yashpatel26@gmail.com>
2022-07-13 17:36:50 +0530

    c

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import hotBg from "./assets/hot.jpg";
 import coldBg from "./assets/cold.jpg";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City..." />
+            <button>°F</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img
+                src="https://openweathermap.org/img/wn/02d@2x.png"
+                alt="weatherIcon"
+              />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+
+          {/* bottom description */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
